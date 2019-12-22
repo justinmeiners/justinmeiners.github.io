3688f1ab5890763e9393cb11358241a93c0d8276
Justin Meiners <justin@justinbsd.my.domain>
2019-12-22 15:57:11 -0700

    upgrade rss

--- template/build.sh
+++ template/build.sh
@@ -1,29 +1,78 @@
-#!/bin/bash
+#!/bin/sh
+
 
 cd ../
-MD=$(find . -type f -name '*.md')
 
-for FN in $MD; do 
-    LINE=$(head -n 1 $FN)
+MD=$(find * -type f -name '*.md')
+
+for FN in $MD
+do 
+    # build HTML
+
+    TITLE=$(head -n 1 $FN)
     HN="$(dirname $FN)/index.html"
-    cat "template/prefix.html" | sed "s/\$TITLE/${LINE}/g" > $HN
+    cat "template/prefix.html" | sed "s/\$TITLE/$TITLE/g" > $HN
     markdown $FN >> $HN
     cat "template/suffix.html" >> $HN
 done
 
-# RSS and home feed
 
+# build rss feed
 FEED="feed.xml"
+SITE="https://justinmeiners.github.io"
 
 echo "<?xml version=\"1.0\" encoding=\"UTF-8\" ?>"  > $FEED
 echo "<rss version=\"2.0\">" >> $FEED
 echo "<channel>" >> $FEED
-cat "template/channel.xml" >> $FEED
-echo "<lastBuildDate>$(date -R)</lastBuildDate>" >> $FEED
+echo "<title>Justin Meiners</title>" >> $FEED
+echo "<description>Welcome to my personal site about programming, mathematics, and philosophy!</description>" >> $FEED
+echo "<link>https://justinmeiners.github.io</link>" >> $FEED
+echo "<lastBuildDate>$(date +"%a, %d %b %Y %T %z")</lastBuildDate>" >> $FEED
 
-cat "template/articles.xml" >> $FEED
+
+
+IFS=,
+
+while read LINE
+do
+    echo "<item>" >> $FEED
+    echo -n "["
+
+    COL=0
+    for PART in $LINE
+    do
+        if [ $COL -eq 0 ]
+        then
+            PUB=$(date -j -f "%m/%d/%Y"  "$PART" +"%a, %d %b %Y %T %z")
+            echo "<pubDate>$PUB</pubDate>" >> $FEED
+            echo -n "$PART - "
+        elif [ $COL -eq 1 ]
+        then
+            echo "<title>$PART</title>" >> $FEED
+            echo -n "$PART]"
+        elif [ $COL -eq 2 ]
+        then
+            if expr "$PART" : "http" > /dev/null
+            then
+                echo "<guid isPermaLink=\"true\">$PART</guid>" >> $FEED
+            else
+                echo "<guid isPermaLink=\"true\">$SITE/$PART</guid>" >> $FEED
+
+                CONTENTS=$(cat $PART/README.md)
+                echo "<description><![CDATA[ \n $CONTENTS \n ]]></description>" >> $FEED
+            fi
+            echo -n "($PART)"
+        fi
+        echo ""
+
+        COL=$(($COL + 1))
+    done
+
+    echo "</item>" >> $FEED
+done < template/links
+
+IFS=$SAVE
+
 echo "</channel>" >> $FEED
 echo "</rss>" >> $FEED
-
-#cat "template/articles.xml" | ./template/linkformat.sh 
 
