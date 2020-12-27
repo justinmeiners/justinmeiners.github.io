cdffecdd639348349220c18cfe66c3048aa33f4c
Justin Meiners <justin.meiners@gmail.com>
2020-12-27 11:39:42 -0700

    fix rss feed. new python generator

--- build.sh
+++ build.sh
@@ -1,74 +1,29 @@
 #!/bin/sh
 
-MD=$(find * -type f -name '*.md')
+ALL_MD=$(find * -type f -name '*.md')
 
-for FN in $MD
+for MD in $ALL_MD
 do 
     # build HTML
 
-    TITLE=$(head -n 1 $FN)
-    HN="$(dirname $FN)/index.html"
-    cat "template/prefix.html" | sed "s/\$TITLE/$TITLE/g" > $HN
-    markdown $FN >> $HN
-    cat "template/suffix.html" >> $HN
+    TITLE=$(head -n 1 $MD)
+    BODY_HTML="$(dirname $MD)/README.html"
+    HTML="$(dirname $MD)/index.html"
+
+    markdown $MD >> $BODY_HTML
+
+    export TITLE
+    cat "template/prefix.html" | envsubst '$TITLE' > $HTML
+    cat $BODY_HTML >> $HTML
+    cat "template/suffix.html" >> $HTML
 done
 
 
 # build rss feed
-FEED="feed.xml"
-SITE="https://justinmeiners.github.io"
+python3 template/build_rss.py > "feed.xml"
 
-echo "<?xml version=\"1.0\" encoding=\"UTF-8\" ?>"  > $FEED
-echo "<rss version=\"2.0\">" >> $FEED
-echo "<channel>" >> $FEED
-echo "<title>Justin Meiners</title>" >> $FEED
-echo "<description>Welcome to my personal site about programming, mathematics, and philosophy!</description>" >> $FEED
-echo "<link>https://justinmeiners.github.io</link>" >> $FEED
-echo "<lastBuildDate>$(date +"%a, %d %b %Y %T %z")</lastBuildDate>" >> $FEED
-
-
-IFS=,
-
-while read LINE
-do
-    echo "<item>" >> $FEED
-    echo -n "["
-
-    COL=0
-    for PART in $LINE
-    do
-        if [ $COL -eq 0 ]
-        then
-            PUB=$(date -j -f "%m/%d/%Y"  "$PART" +"%a, %d %b %Y 00:00:00 -0600")
-            echo "<pubDate>$PUB</pubDate>" >> $FEED
-            echo -n "$PART - "
-        elif [ $COL -eq 1 ]
-        then
-            echo "<title>$PART</title>" >> $FEED
-            echo -n "$PART]"
-        elif [ $COL -eq 2 ]
-        then
-            if expr "$PART" : "http" > /dev/null
-            then
-                echo "<guid isPermaLink=\"true\">$PART</guid>" >> $FEED
-            else
-                echo "<guid isPermaLink=\"true\">$SITE/$PART</guid>" >> $FEED
-
-                CONTENTS=$(cat $PART/README.md | markdown)
-                echo "<description><![CDATA[ \n $CONTENTS \n ]]></description>" >> $FEED
-            fi
-            echo -n "($PART)"
-        fi
-        echo ""
-
-        COL=$(($COL + 1))
-    done
-
-    echo "</item>" >> $FEED
-done < template/links
-
-IFS=$SAVE
-
-echo "</channel>" >> $FEED
-echo "</rss>" >> $FEED
-
+for MD in $ALL_MD
+do 
+    BODY_HTML="$(dirname $MD)/README.html"
+    rm $BODY_HTML
+done
---
+++ template/build_rss.py
@@ -0,0 +1,49 @@
+import sys
+import re
+from datetime import datetime
+from operator import itemgetter
+
+RFC_FORMAT="%a, %d %b %Y %H:%M:%S %z"
+SITE='https://justinmeiners.github.io/'
+
+entries = []
+with open("README.md", 'r') as f:
+    for line in f:
+        m = re.search(r'\[([0-9\/]+) - ([^\]]+)\]\(([^\)]+)', line)
+        if m:
+            date = datetime.strptime(m[1], '%m/%d/%Y')
+            entries.append((date, m[2], m[3]))
+
+entries.sort(reverse=True, key=itemgetter(0))
+w = sys.stdout.write
+
+w('<?xml version="1.0" encoding="UTF-8" ?>\n')
+w('<rss version="2.0">\n')
+w('<title>Justin Meiners Site</title>\n')
+w('<description>Welcome to my personal site about programming, math, and philosophy!</description>\n')
+w('<link>{0}</link>\n'.format(SITE))
+w('<lastBuildDate>{0}</lastBuildDate>\n'.format(datetime.now().strftime(RFC_FORMAT)))
+
+for e in entries:
+    w('<item>\n')
+    w('<pubDate>{0}</pubDate>\n<title>{1}</title>\n'.format(e[0].strftime(RFC_FORMAT), e[1]))
+
+    url = e[2]
+    description = None
+
+    if not e[2].startswith('http'):
+        url = e[2]
+        with open(e[2] + '/README.html', 'r') as cf:
+            description = cf.read()
+
+    w('<guid isPermaLink="true">{0}</guid>\n'.format(url))
+
+    if description:
+        sys.stdout.write('<description><![CDATA[ \n{0}\n ]]></description>\n'.format(description))
+
+    w('</item>\n')
+
+w('</channel></rss>')
+
+
+
